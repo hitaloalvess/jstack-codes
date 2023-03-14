e5c772ab536b74b4e576d91adb9520121735c2f9
hitaloalvess <hitalo.ralves@outlook.com>
2023-03-14 20:34:34 -0300

    feat/mycontacts: create database connection

---
+++ mycontacts/src/database/index.js
@@ -0,0 +1,17 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'root',
+  password: 'root',
+  database: 'mycontacts',
+});
+
+client.connect();
+
+exports.query = async (query) => {
+  const { rows } = client.query(query);
+
+  return rows;
+};
