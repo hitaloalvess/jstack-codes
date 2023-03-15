40b07fae583dbd7418c5a068d356f6d3980f1b8d
hitaloalvess <hitalo.ralves@outlook.com>
2023-03-15 20:46:55 -0300

    refactor: mycontacts/change methods to work with postgres database

--- mycontacts/src/app/controllers/ContactController.js
+++ mycontacts/src/app/controllers/ContactController.js
@@ -3,7 +3,9 @@ const ContactRepository = require('../repositories/ContactRepository');
 class ContactController {
   async index(req, res) {
     // Listar todos os registro
-    const contacts = await ContactRepository.findAll();
+    const { orderBy } = req.query;
+
+    const contacts = await ContactRepository.findAll(orderBy);
 
     res.json(contacts);
   }
@@ -90,13 +92,6 @@ class ContactController {
   async delete(req, res) {
     // Deletar um registro
     const { id } = req.params;
-
-    const contact = await ContactRepository.findById(id);
-
-    if (!contact) {
-      // 404 Not found
-      return res.status(404).json({ error: 'Contact not found!' });
-    }
 
     await ContactRepository.delete(id);
 
